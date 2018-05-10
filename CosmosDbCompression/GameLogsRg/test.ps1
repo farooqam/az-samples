5db67197f0997b095cc06d63b6fa08b177fb903d
farooqam <farooq_am@hotmail.com>
2018-05-10 08:52:26 -0700

    powershell script

---
+++ CosmosDbCompression/GameLogsRg/test.ps1
@@ -0,0 +1,27 @@
+﻿[CmdletBinding()]
+
+Param(
+    [Parameter(Mandatory=$True, Position=1)]
+    [string] $cosmosDbEndpoint,
+
+    [Parameter(Mandatory=$True, Position=2)]
+    [string] $cosmosDbKey,
+
+    [Parameter(Mandatory=$True, Position=3)]
+    [string] $databaseName,
+
+    [Parameter(Mandatory=$True, Position=4)]
+    [string] $resourceGroupName,
+
+    [Parameter(Mandatory=$True, Position=5)]
+    [string] $dependentAssemblyFolder
+)
+
+Invoke-RestMethod -Method Post 
+
+[System.Reflection.Assembly]::Load([System.IO.File]::ReadAllBytes([System.IO.Path]::Combine($dependentAssemblyFolder, "Newtonsoft.Json.dll"))) | Out-Null
+[System.Reflection.Assembly]::Load([System.IO.File]::ReadAllBytes([System.IO.Path]::Combine($dependentAssemblyFolder, "Microsoft.Azure.DocumentDB.Core.dll"))) | Out-Null
+
+
+$uri = New-Object System.Uri($cosmosDbEndpoint)
+New-Object Microsoft.Azure.Documents.Client.DocumentClient($uri, $cosmosDbKey, $null, $null)
