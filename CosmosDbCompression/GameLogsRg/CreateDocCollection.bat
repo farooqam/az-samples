55f7ab6e2979816a2470ea8b511edfbe98b5c5f3
farooqam <farooq_am@hotmail.com>
2018-05-09 18:37:42 -0700

    Batch file update.

--- CosmosDbCompression/GameLogsRg/CreateDocCollection.bat
+++ CosmosDbCompression/GameLogsRg/CreateDocCollection.bat
@@ -2,10 +2,10 @@ SET resourceGroupName="gamelogs"
 SET name="baseballfm"
 SET databaseName="baseball"
 SET collectionName="gamelog"
-SET key="uO4qhxp68Igt5kA13lNpMYsUIU3yjQBaqEIG94bDoaUhoTTvaFu0BSKdw9idLEIhwGVQRJvWkDdjn5n9Q4WLNg=="
-SET url="https://baseballfm.documents.azure.com:443/"
+SET key="key"
+SET endpoint="cosmos db endpoint"
 
-CALL az cosmosdb database delete --db-name %databaseName% --key %key% --name %name% --resource-group-name %resourceGroupName% --url-connection %url%
+CALL az cosmosdb database delete --db-name %databaseName% --key %key% --name %name% --resource-group-name %resourceGroupName% --url-connection %endpoint%
 CALL az cosmosdb database create --name %name% --db-name %databaseName% --resource-group %resourceGroupName%
 CALL az cosmosdb collection create --collection-name %collectionName% --name %name% --db-name %databaseName% --resource-group %resourceGroupName%
 
