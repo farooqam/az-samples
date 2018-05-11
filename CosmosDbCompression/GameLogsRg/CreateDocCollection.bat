fb6775af5eeee1b6069552391bb157d6a2e6a49d
Farooq Mahmud <farooq@techniqly.com>
2018-05-11 15:14:39 -0700

    Cosmos db creation script.

--- CosmosDbCompression/GameLogsRg/CreateDocCollection.bat
+++ CosmosDbCompression/GameLogsRg/CreateDocCollection.bat
@@ -1,9 +1,9 @@
-SET resourceGroupName="gamelogs"
-SET name="baseballfm"
-SET databaseName="baseball"
-SET collectionName="gamelog"
-SET key="key"
-SET endpoint="cosmos db endpoint"
+SET resourceGroupName=""
+SET name=""
+SET databaseName=""
+SET collectionName=""
+SET key=""
+SET endpoint=""
 
 CALL az cosmosdb database delete --db-name %databaseName% --key %key% --name %name% --resource-group-name %resourceGroupName% --url-connection %endpoint%
 CALL az cosmosdb database create --name %name% --db-name %databaseName% --resource-group %resourceGroupName%
